45b911892f6777bf4d025db2f4c0d7d76ba0c746
Skyvorobey <Skyvorobey@icloud.com>
2021-07-09 15:30:00 +0200

    add logical task1,2,3

---
+++ homework/part1/2.4/js.js
@@ -0,0 +1,19 @@
+"use strict"
+
+//1 Работа с переменными
+let admin;
+let name;
+
+name = "Джон";
+admin = name;
+alert(admin);
+
+//2 Придумайте правильные имена
+let ourPlanetName = "Earth";
+let currentUserName = name;
+
+//3 Какие буквы (заглавные или строчные) использовать для имён констант?
+const BIRTHDAY = "18.04.1982";
+
+const age = someCode(BIRTHDAY);
+
